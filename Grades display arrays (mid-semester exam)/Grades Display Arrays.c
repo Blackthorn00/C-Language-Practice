4a8c00c96257b8e95ef3da5e8fad3f31143d87fe
Blackthorn00 <vagelismil@outlook.com>
2023-12-13 01:45:08 +0200

    Updated the application with string arrays.

--- Grades display arrays (mid-semester exam)/Grades Display Arrays.c
+++ Grades display arrays (mid-semester exam)/Grades Display Arrays.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 #define ROW 10
 #define COL 2
 
 
-main ()
-{
-	int grades[ROW][COL], i, j, sum, count=1; // Declare grades array
-	float final[ROW], average, max; // Declare final (average) grades array, average and maximum variables
 
-	
+void main (void)
+{
+	int grades[ROW][COL], i, j, sum, count=1; 
+	float final[ROW], average, max;
+	char exam[] = "written\0";
 	// Get the grades from the keyboard
 	for (i=0; i<ROW; i++)
-	{
-		do // Input check
-		{
-			printf("Enter oral exam grade for student #%d: ", count);
-			scanf("%d", &grades[i][0]);
-			if(grades[i][0]<0 || grades[i][0]>100)
-			{
-				printf("Invalid grade value. Try again\n\n");
-			}
-		}while(!(grades[i][0]>=0 && grades[i][0]<=100));
-		
-		for(j=1; j<COL; j++)
+	{	
+		for(j=0; j<COL; j++)
 		{
 			do // Input check
 			{
-				printf("Enter written exam grade for student #%d: ", count);
+				if(j==0)
+				{
+					strcpy(exam, "written");
+				}else if(j==1)
+				{
+					strcpy(exam, "oral");
+				}
+				printf("Enter %s exam grade for student #%d: ", exam, count);
 				scanf("%d", &grades[i][j]);
 				if(grades[i][j]<0 || grades[i][j]>100)
 				{
@@ -50,17 +48,17 @@ main ()
 			count=i+1;
 		}
 	}
-	printf("\n*******************\nThe maximum average grade is %4.2f, from student #%d\n\n", max, count);
-	
+	printf("\n*******************\nThe maximum final grade is %4.2f, from student #%d\n\n", max, count);
 	
 	
 	//Output grades array
+	printf("            Oral  Written\n");
 	for(i=0; i<ROW; i++)
 	{
 		printf("Student #%d: ", i+1);
 		for(j=0; j<COL; j++)
 		{
-			printf("%d ", grades[i][j]);
+			printf("%d    ", grades[i][j]);
 		}
 		printf("\n");
 	}
@@ -69,9 +67,7 @@ main ()
 	// Output final array
 	for(i=0; i<ROW; i++)
 	{
-		printf("Student #%d: %4.2f\n", i+1, final[i]);
+		printf("Student #%d final grade: %4.2f\n", i+1, final[i]);
 	} 
 	
-	
-	system("pause");
 }
